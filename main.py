e8c1ef9bdffabc372ba6a25e7dadb8ed11da665b
Kirill Smirnov <65505901+terribleMOTHMAN@users.noreply.github.com>
2023-09-25 21:58:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,82 @@
-from jsthon import JsthonDb
 from fastapi import FastAPI
 import uvicorn
 import bcrypt
 import models
+from motor.motor_asyncio import AsyncIOMotorClient
+from envparse import Env
+from starlette.requests import Request
 
-
-api = FastAPI(title="cyberzonetask")
-db = JsthonDb('reservations.json')
+env = Env()
+MONGODB_URL = env.str("MONGODB_URL", default="mongodb://localhost:27017/reservation_database")
+client = AsyncIOMotorClient(MONGODB_URL)
+api = FastAPI()
+api.state.mongo_client = client
 
 
 @api.post('/user/create')
-def user_create(user: models.User):
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(bytes(user.password,"UTF-8"), salt)
-    user.password = str(hashed)
-    db.choose_table('user')
-    db.add(user.model_dump())
-    db.choose_table(None)
-    return {'SUCCES': user}
+async def user_create(user: models.User, request: Request):
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["reservation_database"]
+    cursor = mongo_client.users.find({}).to_list(length=1000)
+    fl = True
+    for doc in await cursor:
+        if doc["id"] == user.id:
+            fl = False
+            break
+    if fl:
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(bytes(user.password,"UTF-8"), salt)
+        user.password = str(hashed)
+        await mongo_client.users.insert_one(user.model_dump())
+        return {"SUCCES": True}
+    else:
+        return {"SUCCES": False}
 
 
 @api.post('/user/delete')
-def user_delete(index: int):
-    db.choose_table('user')
-
-    def func1(data):
-        if data['id'] == index:
-            return True
-
-    db.delete_with_function(func1)
-
-    db.choose_table('booking')
-
-    def func2(data):
-        if data['user_id'] == index:
-            return True
-
-    db.delete_with_function(func2)
-
-    db.choose_table(None)
+async def user_delete(index: int, request: Request):
+    mongo_users: AsyncIOMotorClient = request.app.state.mongo_client["reservation_database"]
+    await mongo_users.users.delete_one({"id": index})
+    await mongo_users.booking.delete_many({"user_id": index})
     return {"SUCCES": True}
 
 
 @api.post('/booking/create')
-def booking_create(user: models.Booking):
-    db.choose_table('booking')
-    db.add(user.model_dump())
-    db.choose_table(None)
-    return {'SUCCES': user}
+async def booking_create(book: models.Booking, request: Request):
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["reservation_database"]
+    cursor = mongo_client.book.find({}).to_list(length=1000)
+    fl1 = True
+    for doc in await cursor:
+        if doc["id"] == book.id:
+            fl1 = False
+            break
+    cursor = mongo_client.users.find({}).to_list(length=1000)
+    fl2 = False
+    for doc in await cursor:
+        if doc["id"] == book.user_id:
+            fl2 = True
+            break
+    if fl1 and fl2:
+        await mongo_client.booking.insert_one(book.model_dump())
+        return {'SUCCES': False}
+    else:
+        return {"SUCCES": False}
 
 
 @api.post('/booking/delete')
-def booking_delete(index: int):
-    db.choose_table('booking')
-
-    def func2(data):
-        if data['id'] == index:
-            return True
-
-    db.delete_with_function(func2)
-
-    db.choose_table(None)
+async def booking_delete(index: int, request: Request):
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["reservation_database"]
+    await mongo_client.booking.delete_one({"id": index})
     return {"SUCCES": True}
 
 
 @api.get('/booking/get')
-def booking_get():
-    db.choose_table('booking')
-    booking_list = db.show_table()
-    booking_list.pop(0)
-    db.choose_table(None)
+async def booking_get(request: Request):
+    booking_list = []
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["test_database"]
+    cursor = mongo_client.booking.find({})
+    for document in await cursor.to_list(length=100):
+        document["_id"] = str(document["_id"])
+        booking_list.append(document)
     return {"SUCCES": booking_list}
 
 if __name__ == "__main__":
